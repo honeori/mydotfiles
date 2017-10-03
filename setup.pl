ad579b49ef83268c985e433ff6df3b72165d795d
nakano <nakano@local.vm.nakano>
2017-10-03 21:29:07 +0900

    update setup.pl: copy file smartly

--- setup.pl
+++ setup.pl
@@ -3,6 +3,29 @@
 use strict;
 use warnings;
 
-rename 'vimrc', '.vimrc';
-rename 'vim', '.vim';
-rename 'bashrc', '.bashrc';
+use File::Copy;
+
+my @HIDDEN_FILES = qw(
+    vimrc
+    bashrc
+    tmux.conf
+);
+
+main();
+
+sub main {
+    ensure_git_installed();
+    copy_hidden_files();
+}
+
+sub ensure_git_installed {
+    system 'which git' or die 'git is required';
+}
+
+sub copy_hidden_files {
+    foreach my $src_file (@HIDDEN_FILES) {
+        my $dst_file = sprintf('~/.%s', $src_file);
+        copy $src_file, $dst_file;
+    }
+}
+
