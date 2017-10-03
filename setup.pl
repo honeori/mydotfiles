dc6b7a256aceea74fac00f00c8450ed4fa8459a6
nakano <nakano@local.vm.nakano>
2017-10-03 21:41:12 +0900

    add setting vim part

--- setup.pl
+++ setup.pl
@@ -11,11 +11,13 @@ my @HIDDEN_FILES = qw(
     tmux.conf
 );
 
-main();
+mkdir 'hoge';
+#main();
 
 sub main {
     ensure_git_installed();
-    copy_hidden_files();
+    #copy_hidden_files();
+    setup_vim();
 }
 
 sub ensure_git_installed {
@@ -29,3 +31,16 @@ sub copy_hidden_files {
     }
 }
 
+sub setup_vim {
+    # copy vim setting directory
+    my $src = 'vim';
+    system "cp -r $src ~/";
+    my $dst = sprintf('~/.%s.test', $src);
+    my $bundle_dir = "$dst/bundle";
+
+    mkdir $bundle_dir;
+    system "git clone git://github.com/Shougo/neobundle.vim $bundle_dir/neobundle.vim";
+
+    print "you need to execute following vim command\n";
+    print ":NeoBundleInstall";
+}
